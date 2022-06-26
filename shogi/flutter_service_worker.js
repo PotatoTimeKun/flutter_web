59f7d341d99e4fd4ff9a8c4948e6f6fc8964b6aa
PotatoTimeKun <ichikota8440@gmail.com>
2022-06-26 11:07:40 +0900

    update shogi:split file

--- shogi/flutter_service_worker.js
+++ shogi/flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "05c112709b701a8a5a9614f0fd310034",
-"/": "05c112709b701a8a5a9614f0fd310034",
-"main.dart.js": "25374d11a3331ca72c33c576abfcedf0",
+"index.html": "a8bd1631db6256f2f5c33f757bd0694c",
+"/": "a8bd1631db6256f2f5c33f757bd0694c",
+"main.dart.js": "842ccfffa7d7c3350b8d0c4e775a40a7",
 "manifest.json": "1a21aa10bcbaf37add3aaa71c85a3489",
 "version.json": "072d32df0cec8a094041bf102ad6cd1e"
 };
